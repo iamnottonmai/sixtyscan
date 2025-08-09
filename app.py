cc32d43e3dffacc6e0661dd71b2e6dbe790b1cb7
iamnottonmai <t.eakanat.seedang@gmail.com>
2025-08-09 23:39:15 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+import streamlit as st
+
+# Import everything from your starting page
+from streamlit_app import *
+
+# If streamlit_app.py has a main function, call it:
+# from streamlit_app import main
+# if __name__ == "__main__":
+#     main()
