878dc0b636cf68cad0df8461c63ec3a88b90e9e2
iamnottonmai <t.eakanat.seedang@gmail.com>
2025-08-09 23:54:34 +0700

    Delete pages directory

--- pages/analysis.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Analysis Test")
-
-st.title("Analysis Page Works!")
-st.write("This is your analysis page.")
-
-if st.button("Back to Home"):
-    st.switch_page("app.py")
