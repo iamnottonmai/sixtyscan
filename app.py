3333a30a35d90670084cd2d673aae9b215db6e67
iamnottonmai <t.eakanat.seedang@gmail.com>
2025-08-09 23:28:09 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py (in root)
+import sys
+import os
+sys.path.append('src')  # or whatever your subdirectory is called
+from your_actual_app_file import *  # or import main components
+
+if __name__ == "__main__":
+    # Run your app
